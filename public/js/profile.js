8e11b2b62973274e87d9cc7c49bb2e73b1efc736
jenyo21j <111152632+jenyo21j@users.noreply.github.com>
2023-02-14 10:58:51 +0800

    table created in mysql

--- public/js/profile.js
+++ public/js/profile.js
@@ -6,9 +6,9 @@ const newFormHandler = async (event) => {
     const description = document.querySelector('#genre').value.trim();
   
     if (name && artist && genre) {
-      const response = await fetch(`/api/songs`, {
+      const response = await fetch(`/api/song`, {
         method: 'POST',
-        body: JSON.stringify({ song, artist, genre }),
+        body: JSON.stringify({ name, artist, genre }),
         headers: {
           'Content-Type': 'application/json',
         },
@@ -26,7 +26,7 @@ const newFormHandler = async (event) => {
     if (event.target.hasAttribute('data-id')) {
       const id = event.target.getAttribute('data-id');
   
-      const response = await fetch(`/api/songs/${id}`, {
+      const response = await fetch(`/api/song/${id}`, {
         method: 'DELETE',
       });
   
--- server.js
+++ server.js
@@ -6,19 +6,17 @@ const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
-// const { restore } = require('./models/User');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-// const router = app.Router();
 
 const hbs = expressHbs.create({helpers});
 
 const sess = {
     secret: 'Super secret secret',
-    cooke:{
+    cookie: {
         maxAge: 300000,
         httpOnly: true,
         secure: false,
@@ -39,25 +37,13 @@ app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-// router.get('/', (req, res) => {
-//     //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
-//     res.render('main', {layout : 'index'});
-//     });
-
-// router.get('/login', (req, res) => {
-//     // login logic 
-//     res.render('login');
-// });
-
-
-// router.get('/static', (req, res) => {
-//     res.render("static");
-// });
 
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 
 app.use(routes);
@@ -66,12 +52,4 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening at http://localhost:${PORT}`));
   });
-
-
-  // app.use(express.static(path.join(__dirname, 'public')));
-// app.set('views', path.join(__dirname, 'views'));
-// app.get('/', (req, res) => {
-//     //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
-//     res.render('main', {layout : 'index'});
-//     });
 
